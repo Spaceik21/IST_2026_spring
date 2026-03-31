0ea54bcf8c9a4f389106d58ea044e65f9e9b6942
Spaceik21 <toporowadarya@yandex.ru>
2026-03-31 16:01:24 +0300

    Add rsa.py and test_rsa.py

---
+++ homework-02/rsa.py
@@ -0,0 +1,82 @@
+import random
+import typing as tp
+
+def is_prime(n):
+  if n < 1 or n%2 == 0:
+    return False
+  if n == 1:
+    return True
+  for i in range(3, n//2 + 1, 2):
+    if n % i == 0:
+      return False
+      break
+    elif n % i != 0:
+      i+=1
+  return True
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def multiplicative_inverse(e, phi):
+    if phi == 1:
+        return 0
+    t, new_t = 0, 1
+    r, new_r = phi, e
+    while new_r != 0:
+        q = r // new_r
+        t, new_t = new_t, t - q * new_t
+        r, new_r = new_r, r - q * new_r
+    if r > 1:
+        return 0
+    if t < 0:
+        t += phi
+    return t
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError("p and q cannot be equal")
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    d = multiplicative_inverse(e, phi)
+
+    return ((e, n), (d, n))
+
+
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    return "".join(plain)
+
+
+if __name__ == "__main__":
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
